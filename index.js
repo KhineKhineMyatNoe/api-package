a3494e0f554a7196a296916344d5b77a40a67c57
Khine Khine Myat Noe <khinekhinemyatnoe99@gmail.com>
2023-02-11 13:54:15 +0630

    my first dependencies

---
+++ index.js
@@ -0,0 +1,22 @@
+const sampleData = [
+    {
+    name: "cat ma",
+    value: "cat_ma"
+},{
+    name: "atom",
+    value: "atom_atom"
+},{
+    name: "khine",
+    value: "khine_khine"
+},
+{
+    name: "atom",
+    value: "atom1"
+},{
+    name: "myat",
+    value: "myat_myat"
+},{
+    name: "noe",
+    value: "noe_noe"
+}
+]
